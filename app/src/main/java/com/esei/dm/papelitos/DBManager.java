71d701a260b835b7a07a6310656899512e25f581
bdvazquez17 <bdvazquez17@esei.uvigo.es>
2021-11-16 13:17:48 +0100

    16/11

--- app/src/main/java/com/esei/dm/papelitos/DBManager.java
+++ app/src/main/java/com/esei/dm/papelitos/DBManager.java
@@ -18,14 +18,14 @@ public class DBManager extends SQLiteOpenHelper {
     /*Campos de tabla JUGADOR*/
     public static String JUGADOR_id = "id_jugador";
     public static String JUGADOR_nombre = "nombre";
+    public static String EQUIPO_id_fk = "id_equipo";
 
     /*Campos tabla EQUIPO, existe relacion 1 a n entre equipo y jugadores*/
     public static String EQUIPO_id = "id_equipo";
-    public static String JUGADOR_id_fk = "id_jugador";
     public static String EQUIPO_nombre = "equipo_nombre";
 
     /*Campos tabla PUNTUACION*/
-    public static String EQUIPO_id_fk = "id_equipo";
+    public static String EQUIPO_id_fk2 = "id_equipo";
     public static String puntuacion = "punt";
 
     public DBManager(Context context){
@@ -42,7 +42,8 @@ public class DBManager extends SQLiteOpenHelper {
             db.beginTransaction();
             db.execSQL("CREATE TABLE IF NOT EXISTS " + tabla_jugador + "("
                     + JUGADOR_id + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + JUGADOR_nombre + " TEXT NOT NULL"
+                    + JUGADOR_nombre + " TEXT NOT NULL,"
+                    + EQUIPO_id_fk + " INTEGER NOT NULL , FOREIGN KEY ("+EQUIPO_id_fk+") REFERENCES " + tabla_equipo + "("+EQUIPO_id+") ON DELETE CASCADE"
                     + ")"
             );
             db.setTransactionSuccessful();
@@ -57,8 +58,7 @@ public class DBManager extends SQLiteOpenHelper {
             db.beginTransaction();
             db.execSQL("CREATE TABLE IF NOT EXISTS " + tabla_equipo + "("
                     + EQUIPO_id + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-                    + EQUIPO_nombre + " TEXT NOT NULL,"
-                    + JUGADOR_id_fk +" INTEGER NOT NULL , FOREIGN KEY ("+JUGADOR_id_fk+") REFERENCES " + tabla_jugador + "("+JUGADOR_id+") ON DELETE CASCADE"
+                    + EQUIPO_nombre + " TEXT NOT NULL"
                     + ")"
             );
             db.setTransactionSuccessful();
@@ -199,19 +199,19 @@ public class DBManager extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
 
-        values.put(EQUIPO_id, id_equipo);
-        values.put(JUGADOR_id_fk, id_jugador);
+        values.put(JUGADOR_id, id_jugador);
+        values.put(EQUIPO_id_fk, id_equipo);
 
         try{
             db.beginTransaction();
-            cursor = db.query(tabla_equipo, null, EQUIPO_id + "=? AND " + JUGADOR_id_fk
-                    + "=?", new String[]{id_equipo, id_jugador}, null, null, null, null);
+            cursor = db.query(tabla_jugador, null, JUGADOR_id + "=? AND " + EQUIPO_id_fk
+                    + "=?", new String[]{id_jugador, id_equipo}, null, null, null, null);
             if(cursor.getCount() > 0){
-                db.update(tabla_equipo, values, EQUIPO_id + "=? AND " + JUGADOR_id_fk
-                        + "=?", new String[]{id_equipo, id_jugador});
+                db.update(tabla_jugador, values, JUGADOR_id + "=? AND " + EQUIPO_id_fk
+                        + "=?", new String[]{id_jugador, id_equipo});
             }
             else{
-                db.insert(tabla_equipo, null, values);
+                db.insert(tabla_jugador, null, values);
             }
             db.setTransactionSuccessful();
             toret = true;
@@ -231,7 +231,7 @@ public class DBManager extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         try {
             db.beginTransaction();
-            db.delete(tabla_equipo, EQUIPO_id + "=? AND "+JUGADOR_id_fk+" =?", new String[]{id_equipo, id_jugador});
+            db.delete(tabla_jugador, JUGADOR_id + "=? AND "+EQUIPO_id_fk+" =?", new String[]{id_jugador, id_equipo});
             db.setTransactionSuccessful();
             toret = true;
         }catch(SQLException exc){
